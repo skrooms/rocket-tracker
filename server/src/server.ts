417b610d24145a48c4625d7ee1547d4897270e41
Noah <skrooms@protonmail.com>
2024-03-06 14:53:31 -0500

    implemented env using envalid

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,15 @@
 import app from "./app";
 import mongoose from "mongoose";
+import env from "./util/validateEnv";
 
 
 const start = async () => {
     try {
+        // Connect to the MongoDB database using the connection string contained in the .env file
+        await mongoose.connect(env.MONGO_URI);
+        console.log('Successfully connected to the database');
 
+        // Start the express app only if the connection is sucessful
         app.listen(3000, () => {
             console.log(`Server is listening on port 3000...`);
         })
