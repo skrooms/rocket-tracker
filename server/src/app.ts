79579aa2cab83095511dd850ddcc67744762d224
Noah <skrooms@protonmail.com>
2024-03-26 21:09:32 -0400

    updated routes for deployment, added cors as dependency in server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,17 @@
 import "dotenv/config";
 import express from "express";
 import playersRouter from "./routes/players";
+import cors from "cors";
 
 const app = express();
 
+const corsOptions = {
+    origin: "https://rocket-tracker-client.onrender.com"
+}
+
 // Middleware
 app.use(express.json());
+app.use(cors(corsOptions));
 
 // Routes
 app.use("/api/players", playersRouter);
